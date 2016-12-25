7827b11fbc5bb1f90019cccdc5235203ae9968d1
rohitdec01 <rohitdec01@gmail.com>
2016-12-25 15:25:42 +0530

    Implement config server. for centralized configuration

---
+++ tmDemoConfigServer/src/main/java/com/att/demo/TmDemoConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.att.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TmDemoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmDemoConfigServerApplication.class, args);
+	}
+}
