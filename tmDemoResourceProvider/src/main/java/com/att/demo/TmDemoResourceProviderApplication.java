8a74f51680b669a040fc6d31a3764d4d2a680f3c
rohitdec01 <rohitdec01@gmail.com>
2017-01-04 09:58:35 +0530

    basic of authentication

---
+++ tmDemoResourceProvider/src/main/java/com/att/demo/TmDemoResourceProviderApplication.java
@@ -0,0 +1,24 @@
+package com.att.demo;
+
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class TmDemoResourceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmDemoResourceProviderApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String securedCall() {
+		return "success (id: " + UUID.randomUUID().toString().toUpperCase() + ")";
+	}
+}
